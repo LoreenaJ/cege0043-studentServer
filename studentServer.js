372edfc62bb55b1c863d3a7fe26edaa08a387037
LoreenaJ <loreena.jaouen@gmail.com>
2019-02-28 08:53:14 -0500

    Trying to debug the directories not loading

--- studentServer.js
+++ studentServer.js
@@ -9,14 +9,6 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
-app.get('/:fileName',function(req,res){
-	//run some server-side code
-	var fileName = req.params.fileName;
-	console.log(fileName+' requested');
-	//note that __dirname gives the path to the studentServer.js file
-	res.sendFile(__dirname+'/'+fileName);
-})
-
 // adding functionality to log the requests
 app.use(function(req,res,next){
 	var filename = path.basename(req.url);
@@ -25,6 +17,13 @@ app.use(function(req,res,next){
 	next();
 });
 
-// serve static files - e.g. html, css, js
-// this should always be the last line in the server file
+app.get('/', function (req, res) {
+  	// run some server-side code
+	console.log('the http server has received a request');
+	res.send('Hello World from the http server');
+});
+
+// finally - serve static files for requests that are not met by any of the
+// code above
+// serve static files - e.g. html, css
 app.use(express.static(__dirname));
