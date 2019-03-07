a876695108ea18dd9000515af2d772f92fb31691
LoreenaJ <loreena.jaouen@gmail.com>
2019-03-07 07:34:37 -0500

    Debugging

--- studentServer.js
+++ studentServer.js
@@ -177,15 +177,15 @@ console.log("the colnames "+thecolnames);
 // and create the required geoJSON format using a query adapted from here:
 // http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4thJanuary 2018
 // note that query needs to be a single string with no line breaks sobuilt it up bit by bit
-var querystring = " SELECT 'FeatureCollection' As type,array_to_json(array_agg(f)) As features FROM ";
-querystring = querystring + "(SELECT 'Feature' As type ,ST_AsGeoJSON(lg." + req.params.geomcolumn+")::json As geometry, ";
+var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
+querystring = querystring + "(SELECT 'Feature' As type, ST_AsGeoJSON(lg." + req.params.geomcolumn+")::json As geometry, ";
 querystring = querystring + "row_to_json((SELECT l FROM (SELECT"+colnames + ") As l )) As properties";
 // depending on whether we have a port number, do differen things
 if (req.params.portNumber) {
-querystring = querystring + " FROM "+req.params.tablename+"As lg where lg.port_id = '"+req.params.portNumber + "' limit 100 ) As f ";
+querystring = querystring + " FROM "+req.params.tablename+" As lg where lg.port_id = '"+req.params.portNumber + "' limit 100 ) As f ";
 }
 else {
-querystring = querystring + " FROM "+req.params.tablename+"As lg limit 100 ) As f ";
+querystring = querystring + " FROM "+req.params.tablename+" As lg limit 100 ) As f ";
 }
 console.log(querystring);
 // run the second query
